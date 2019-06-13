1ec8f1acbd123299120d39bc793f09167bda6dde
Breno Felipe <brenofeliped15@live.com>
2019-06-13 15:03:52 -0300

    mario margenta

--- src/test.s
+++ src/test.s
@@ -1,24 +1,31 @@
 .data
-.include "../sprites/teste_mario.s"
-
+.include "../sprites/mario_pe_atras_levantado_virado_esquerda_320x240.s"
 .text
 
 
-
-
-# Carrega a imagem1
-FORA:	li t1,0xFF000000	# endereco inicial da Memoria VGA
+# Preenche a tela de vermelho
+	li t1,0xFF000000	# endereco inicial da Memoria VGA
 	li t2,0xFF012C00	# endereco final 
-	la s1,teste_mario		# endereço dos dados da tela na memoria
+	li t3,0x30303030	# cor vermelho|vermelho|vermelhor|vermelho
+LOOP: 	beq t1,t2,FORA		# Se for o último endereço então sai do loop
+	sw t3,0(t1)		# escreve a word na memória VGA
+	addi t1,t1,4		# soma 4 ao endereço
+	j LOOP			# volta a verificar
+FORA:
+# Carrega a fase1
+PrintFase:	li t1,0xFF000000	# endereco inicial da Memoria VGA
+	li t2,0xFF012C00	# endereco final 
+	la s1,mario_pe_atras_levantado_virado_esquerda_320x240		# endereço dos dados da tela na memoria
 	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
-LOOP1: 	beq t1,t2,FORA1		# Se for o último endereço então sai do loop
+LOOP1: 	beq t1,t2,FIM		# Se for o último endereço então sai do loop
 	lw t3,0(s1)		# le um conjunto de 4 pixels : word
 	sw t3,0(t1)		# escreve a word na memória VGA
 	addi t1,t1,4		# soma 4 ao endereço
 	addi s1,s1,4
 	j LOOP1			# volta a verificar
 
-FORA1:
+    
 # devolve o controle ao sistema operacional
-FIM:	li a7,10		# syscall de exit
-	ecall
+FIM:	#li a7,10		# syscall de exit
+	#ecall
+	j FIM
